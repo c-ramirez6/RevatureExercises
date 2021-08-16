e4e087dda3e029f5b788aa5fa1c455603c8928f3
unknown <chris.ramirez@revature.net>
2021-08-16 17:46:29 -0400

    Front end day5 and servlets

---
+++ revature-servlet/src/main/java/main/MyServlet.java
@@ -0,0 +1,45 @@
+package main;
+
+import java.io.IOException;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+public class MyServlet implements Servlet{
+	ServletConfig config;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		this.config = config;
+		System.out.println("creating object...");
+		
+	}
+	
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+		System.out.println("servicing...");
+		
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("destroying servlet object...");
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		return config;
+	}
+
+	@Override
+	public String getServletInfo() {
+		return "this servlet was created by admin";
+	}
+
+
+
+}
